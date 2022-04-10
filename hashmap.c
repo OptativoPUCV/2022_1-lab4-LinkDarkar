69148afded9c9650857dfb8a6644841a94420740
Your Name <you@example.com>
2022-04-10 11:31:19 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -5,34 +5,38 @@
 #include <ctype.h>
 #include "hashmap.h"
 
-
 typedef struct HashMap HashMap;
 int enlarge_called=0;
 
-struct HashMap {
+struct HashMap
+{
     Pair ** buckets;
     long size; //cantidad de datos/pairs en la tabla
     long capacity; //capacidad de la tabla
     long current; //indice del ultimo dato accedido
 };
 
-Pair * createPair( char * key,  void * value) {
+Pair * createPair( char * key,  void * value)
+{
     Pair * new = (Pair *)malloc(sizeof(Pair));
     new->key = key;
     new->value = value;
     return new;
 }
 
-long hash( char * key, long capacity) {
+long hash( char * key, long capacity)
+{
     unsigned long hash = 0;
-     char * ptr;
-    for (ptr = key; *ptr != '\0'; ptr++) {
+    char * ptr;
+    for (ptr = key; *ptr != '\0'; ptr++)
+    {
         hash += hash*32 + tolower(*ptr);
     }
     return hash%capacity;
 }
 
-int is_equal(void* key1, void* key2){
+int is_equal(void* key1, void* key2)
+{
     if(key1==NULL || key2==NULL) return 0;
     if(strcmp((char*)key1,(char*)key2) == 0) return 1;
     return 0;
@@ -63,7 +67,8 @@ void insertMap(HashMap * map, char * key, void * value)
     map->size += 1;
 }
 
-void enlarge(HashMap * map) {
+void enlarge(HashMap * map)
+{
     enlarge_called = 1; //no borrar (testing purposes)
 
 
@@ -81,23 +86,39 @@ HashMap * createMap(long capacity)
     return array;
 }
 
-void eraseMap(HashMap * map,  char * key) {    
+void eraseMap(HashMap * map,  char * key)
+{    
 
 
 }
 
-Pair * searchMap(HashMap * map,  char * key) {   
+Pair * searchMap(HashMap * map,  char * key)
+{   
+    long index = hash(key , map->capacity);
+    while (map->buckets[index] != NULL && map->buckets[index]->key != NULL)
+    {
+        if (is_equal(key , map->buckets[index]->key) == 1)
+        {
+            return map->buckets[index];
+        }
+        index = (index + 1) % map->capacity;
+        if (map->buckets[index] == NULL)
+        {
+            return NULL;
+        }
+    }
+    
+    return NULL;
+}
 
+Pair * firstMap(HashMap * map)
+{
 
     return NULL;
 }
 
-Pair * firstMap(HashMap * map) {
-
-    return NULL;
-}
-
-Pair * nextMap(HashMap * map) {
+Pair * nextMap(HashMap * map)
+{
 
     return NULL;
 }
