f76fb3bddcffacc78c741fe6374a580d3c211019
Your Name <you@example.com>
2022-04-10 20:25:51 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -72,9 +72,8 @@ void enlarge(HashMap * map)
     Pair ** oldBuckets;
     oldBuckets = map->buckets;
     long oldCapacity = map->capacity;
-    //free(map);
-    map->capacity *= 2;
-    map = createMap(map->capacity);
+    free(map);
+    map = createMap(oldCapacity * 2);
     for (long index = 0 ; index < oldCapacity ; index += 1)
     {
         if (oldBuckets[index] != NULL && oldBuckets[index]->key != NULL)
